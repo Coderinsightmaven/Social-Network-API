eec6bfd92a06b45066f546b1c33e4b16a2f1ec0c
Coderinsightmaven <lmarincik@proton.me>
2023-10-31 10:19:21 -0600

    added freinds list remove and add

--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,55 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+// Define the Reaction schema
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    _id: false, // Prevents creation of an additional _id for the subdocument
+  }
+);
 
+// Define the Thought schema
 const thoughtSchema = new mongoose.Schema({
   thoughtText: {
     type: String,
     required: true,
     minlength: 1,
-    maxlength: 280 // Typical length for a social media post
+    maxlength: 280, // Typical length for a social media post
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   username: {
     type: String,
-    required: true
+    required: true,
   },
-  reactions: [
-    {
-      // Define a Reaction schema here or as a separate model if reactions are complex
-    }
-  ]
+  reactions: [reactionSchema],
 });
 
-const Thought = mongoose.model('Thought', thoughtSchema);
+// Virtual for reactionCount
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
 
 module.exports = Thought;
